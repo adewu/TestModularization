9bdeb4e98ffd4c276c07d5f0baeb1b66558220a2
wuxm <380510218@qq.com>
2016-10-28 17:43:27 +0800

    Initial commit

---
+++ app/src/main/java/io/wuxm/testmodularization/MainActivity.java
@@ -0,0 +1,33 @@
+package io.wuxm.testmodularization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.greenrobot.eventbus.EventBus;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tv)
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        tv.setText("aa");
+    }
+
+    @OnClick(R.id.tv)
+    public void onclick(View view){
+        Toast.makeText(this,"haha",Toast.LENGTH_SHORT).show();
+    }
+}
